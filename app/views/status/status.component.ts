592931e530649069984fd26b39b70ccf24b95a8b
Andrew Smiley <andrew.smiley@ge.com>
2017-03-06 22:10:09 -0500

    fixed bug in gauge and some other small changes

--- app/views/status/status.component.ts
+++ app/views/status/status.component.ts
@@ -15,12 +15,14 @@ export class StatusComponent implements AfterViewInit {
     @ViewChild('gauge') gauge: ElementRef;
     ounces: number = 345;
     temp: number = 35;
-    fullness: number = 100;
+    fullness: number = 100;//this.kegService.getVolume()
     overview: any = {
         "days_in_lines": 5,
         "days_in_keg": 5,
         "result": "true",
-        "days_in_c02": 5
+        "days_in_c02": 5,
+        "keg_percentage": 5
+
     };
     entries: LogEntry [] = [{
             status: 'complete',
@@ -37,13 +39,15 @@ export class StatusComponent implements AfterViewInit {
         this.entries = this.kegService.getLogEntries();
         this.getStatus();
         this.getOverview();
+        console.log("ng After View init")
+       console.log(this.overview)
     }
 
     getStatus() {
         console.info("trying to get the volume");
         this.kegService.getVolume()
                     .subscribe(body => {
-                        this.fullness = body.health.keg.pecentage;
+                        this.fullness = body.health.keg.percentage;
                         this.temp = body.health.keg.temperature;
                         console.info(this.fullness);
                         this.renderGauge();
@@ -59,11 +63,14 @@ export class StatusComponent implements AfterViewInit {
     }
 
     renderGauge() {
-        let x = 90;
+      console.log(this.overview)
+      // this.kegService.getVolume().subscribe(body => {this.fullness= body.health.keg.percentage})
+      console.log(this.fullness);
+      let x = 90;
         let y = 90;
         let radius = 75;
 
-        let degrees = (this.fullness/ 100) * 360;
+        let degrees = ((100-this.fullness)/ 100) * 360;
         let radians = degrees * Math.PI / 180;
         if (this.gauge) {
             let context:  CanvasRenderingContext2D = this.gauge.nativeElement.getContext('2d');
@@ -81,7 +88,7 @@ export class StatusComponent implements AfterViewInit {
            context.stroke();
 
            context.font = '60px Impact, Helvetica, Arial, sans-serif';
-           context.fillText(this.fullness.toString(), x - 37, y + 20);
+           context.fillText((100-this.fullness).toString(), x - 37, y + 20);
 
            context.strokeStyle = 'green';
            context.font = '20px Helvetica, Arial, sans-serif';
